ecde9bd4a2c18eb9f1af9d8bcfd17ff735df0f6d
201311113 <antjdlekd123@naver.com>
2020-08-18 19:00:12 +0900

    Frontend를 만들면서 알게된 추가/삭제/수정할 사항 편집

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -3,13 +3,10 @@ import {generateSecret, sendSecretMail} from "../../../utils";
 
 export default{
     Mutation:{
-        requestSecret: async(_,args,{request})=>{
-            console.log(request);
+        requestSecret: async(_,args)=>{
             const {email} = args;
             const loginSecret = generateSecret();
-            console.log(loginSecret);
             try{
-                throw Error();
                 await sendSecretMail(email,loginSecret);
                 await prisma.updateUser({data:{loginSecret}, where: {email}});
                 return true;
